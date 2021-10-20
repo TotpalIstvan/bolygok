53bccc7439cdda0297b4e6fd73cec28907af0cb3
Juráncsik Máté <jurancsik.mate501@gmail.com>
2021-10-20 15:24:36 +0200

    bolygok

---
+++ src/hu/petrik/bolygok/Main.java
@@ -0,0 +1,18 @@
+package hu.petrik.bolygok;
+
+import bolygo.Bolygo;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	    double sajatsuly = 65;
+        Bolygo fold = Bolygo.FOLD;
+        double sajatTomeg =  sajatsuly/fold.getGravitacio();
+
+        for (Bolygo bolygo: Bolygo.values()
+             ) {
+            System.out.printf("%s (%f): %f\n", bolygo, bolygo.getGravitacio(), bolygo.getSuly(sajatTomeg));
+        }
+    }
+}
